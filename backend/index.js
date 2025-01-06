acb30027a74e05dfc691795c8cf34c84cf9e47df
MaybeRosado <erosado@ucol.mx>
2025-01-06 12:51:27 -0600

    second commit

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'))
 
 let notes = [
       {
@@ -78,7 +79,7 @@ app.post('/api/notes', (request, response) => {
 
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
