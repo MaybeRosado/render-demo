16c543ec6c53b861ff8ba12ce714f594f282a43e
MaybeRosado <erosado@ucol.mx>
2025-01-15 14:25:26 -0600

    Added handling error and put method

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,32 @@
 require('dotenv').config()
-console.log('MONGO_DB:', process.env.MONGODB_URI)
-console.log('PORT:', process.env.PORT)
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const Note = require('./models/note')
-const password = process.argv[2]
 
-app.use(cors());
-app.use(express.json());
+
 app.use(express.static('dist'))
 
-let notes = [
-      {
-        "id": 1,
-        "content": "HTML is easy",
-        "important": true
-      },
-      {
-        "id": 2,
-        "content": "Browser can execute only JavaScript",
-        "important": false
-      },
-      {
-        "id": 3,
-        "content": "GET and POST are the most important methods of HTTP protocol",
-        "important": true
-      }
-    ];
+const errorHadler = (error, request, response, next) => {
+  console.error(error.message)
 
+  if(error.name === 'CastError'){
+    return response.status(400).send({error: 'malformatted id'})
+
+  }else if(error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
+  }
+
+  next(error)
+}
+ 
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+const unknowEndpoint = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+} 
+ 
 app.get('/', (request, response )=> {
     response.send('<h1>Hello World!</h1>')
 });
@@ -39,51 +37,56 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+  .then(note => {
+    if(note){
+      response.json(note)
+    }else{
+      response.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+})
 
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
+app.put('/api/notes/:id', (request, response, next) => {
+  const {content, important} = request.body;
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+     {content, important},
+    {new: true, runValidators:true, context: 'query'})
+    .then(updateNote => {
+      response.json(updateNote)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter(note => note.id !== id);
-
-  response.status(204).end();
+  Note.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 });
 
-const generateId = () => {
-  const maxId = notes.length > 0
-  ? Math.max(...notes.map(n => n.id))
-  : 0
-  return maxId + 1;
-}
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
-  if(!body.content){
-    return response.status(400).json({
-      error:'Information missing'
-    })
-  }
-
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId()
-  }
+    important: body.important || false,
+  })
 
-  notes = notes.concat(note);
-  response.json(note);
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+  .catch(error => next(error))
 })
 
+app.use(unknowEndpoint);
+app.use(errorHadler);
 
 
 const PORT = process.env.PORT 
